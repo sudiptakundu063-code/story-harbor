4841b9757f4a8f60567f1f6ef2546e19b6b8ca6c
Sudipta Kundu <sudiptakundu025@gmail.com>
2026-07-22 11:25:35 +0530

    fix: Add basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,30 @@ import HomePage from "./pages/HomePage"
 import StoryPage from "./pages/StoryPage";
 import ChapterPage from "./pages/ChapterPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/stories/:storyId",
+          element: <StoryPage />,
+        },
+      ],
+    },
+    {
+      path: "/stories/:storyId/chapters/:translationId",
+      element: <ChapterPage />,
+    },
+  ],
   {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/stories/:storyId",
-        element: <StoryPage />,
-      }
-    ],
-  },
-  {
-    path: "/stories/:storyId/chapters/:translationId",
-    element: <ChapterPage />,
+    basename: "/story-harbor",
   }
-]);
+);
 
 function App() {
   return (
